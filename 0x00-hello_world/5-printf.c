dfafe163ffee538f8840037653d1698812805898
sosana <sosana.gaied@gmail.com>
2023-07-14 15:33:01 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - A program that prints a line with the puts function
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
