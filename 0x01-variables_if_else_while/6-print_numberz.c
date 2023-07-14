8dcd4005b1e86bd0527fd12e997aeab687b9cafa
sosana <sosana.gaied@gmail.com>
2023-07-14 16:18:20 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - A program that prints a line with the puts function
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+putchar(n + 48);
+}
+putchar('\n');
+return (0);
+}
